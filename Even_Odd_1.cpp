38e8e0e97bd838dc8a0966d5d4e648ddd70d1489
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-12-05 09:24:14 +0600

    Initial Commit & added 8 problems

---
+++ Even_Odd_1.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int testCase;
+    cin >> testCase;
+    while (testCase--)
+    {
+        int x;
+        cin >> x;
+        if (x % 2 == 0)
+        {
+            cout << "even" << endl;
+        }
+        else
+        {
+            cout << "odd" << endl;
+        }
+    }
+
+    return 0;
+}
